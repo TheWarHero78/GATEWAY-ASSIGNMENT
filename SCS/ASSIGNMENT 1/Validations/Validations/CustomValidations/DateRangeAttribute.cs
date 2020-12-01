b4d4743f559c8a084b2df1456205ee7c74197650
TheWarHero78 <54692481+TheWarHero78@users.noreply.github.com>
2020-12-01 14:25:04 +0530

    Intial commit

---
+++ SCS/ASSIGNMENT 1/Validations/Validations/CustomValidations/DateRangeAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Validations.CustomValidations
+{
+    public class DateRangeAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime dateTime = Convert.ToDateTime(value);
+            bool x = dateTime >= DateTime.Now;
+            return x;
+        }
+    }
+}
---
+++ SCS/ASSIGNMENT 1/Validations/Validations/Models/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Validations.CustomValidations;
+
+namespace Validations.Models
+{
+    
+    public class User
+    {
+    
+        [Display(Name = "User Name")]
+        [Required(ErrorMessage = "Please enter Namw")]
+        public string UserName { get; set; }
+
+        [Display(Name = "User Email")]
+        [Required,EmailAddress]
+        public string UserEmail { get; set; }
+
+        [Display(Name = "Date of Joining")] 
+     
+        public Nullable<System.DateTime> DOJ { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password is Required")]
+        [MinLength(6), MaxLength(10)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Confirm Password is Required")]
+        [Compare("Password", ErrorMessage = "Password and Confirm Password do not match")]
+        [MinLength(6), MaxLength(10)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword  { get; set; }
+
+        [DateRange(ErrorMessage = "Birth Date must be less than to Today's Date")]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfBirth
+        {
+            get;
+            set;
+        }
+    }
+}
